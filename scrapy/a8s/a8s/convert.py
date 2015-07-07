037125da292b6efb68e2af9a48e58a8c5f4c7f32
Fabrice Laporte <kraymer@gmail.com>
2015-07-07 06:58:18 +0200

    remove checkboxes

--- scrapy/a8s/a8s/convert.py
+++ scrapy/a8s/a8s/convert.py
@@ -31,12 +31,11 @@ def get_content(item):
     if 'ingredients' in item:
         res += '### Ingredients\n\n'
         for x in item['ingredients']:
-            res += '  <input type="checkbox"><label>%s</label>\n' % x
+            res += '  %s\n' % x
         res += '\n\n### Recipe\n\n'
     res += 'Go check the recipe instructions on [%s](%s).' % \
         (item['source'], item['link'])
     return res
-
 
 
 if __name__ == "__main__":
